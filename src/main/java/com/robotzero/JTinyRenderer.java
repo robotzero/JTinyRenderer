661b286934b716976e5072701de755f48d4a585b
Agnieszka K. Kocinski <0robotzero@gmail.com>
2021-01-03 15:22:05 +0000

    Stage 1

--- src/main/java/com/robotzero/JTinyRenderer.java
+++ src/main/java/com/robotzero/JTinyRenderer.java
@@ -2,12 +2,18 @@ package com.robotzero;
 
 import com.robotzero.external.TGAReader;
 import com.robotzero.external.TGAWriter;
+import de.javagl.obj.FloatTuple;
+import de.javagl.obj.Obj;
+import de.javagl.obj.ObjFace;
+import de.javagl.obj.ObjReader;
+import org.joml.Vector3f;
 
-import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import static java.awt.image.BufferedImage.TYPE_INT_RGB;
 
@@ -16,13 +22,37 @@ public class JTinyRenderer {
   public static void main(String [] args) {
     String path = "result/image.bmp";
     Render render = new Render();
-    try {
-      int width = 100;
-      int height = 100;
-      BufferedImage image = new BufferedImage(100, 100, TYPE_INT_RGB);
-      render.lineSecond(13, 20, 80, 40, image, Color.WHITE);
-      render.lineSecond(20, 13, 40, 80, image, Color.RED);
-      render.lineSecond(80, 40, 13, 20, image, Color.RED);
+    int width = 800;
+    int height = 800;
+    BufferedImage image = new BufferedImage(width, height, TYPE_INT_RGB);
+
+    // Read an OBJ file
+    try (InputStream objInputStream =
+        new FileInputStream("./data/head.obj")) {
+      Obj model = ObjReader.read(objInputStream);
+
+      for (int i = 0; i < model.getNumFaces(); i++) {
+        ObjFace face = model.getFace(i);
+        for (int j = 0; j < 3; j++) {
+          FloatTuple v0 = model.getVertex(face.getVertexIndex(j));
+          FloatTuple v1 = model.getVertex(face.getVertexIndex((j + 1) % 3));
+          int x0 = (int) ((v0.getX() + 1.0) * width / 2.0);
+          int y0 = (int) ((v0.getY() + 1.0) * height / 2.0);
+          int x1 = (int) ((v1.getX() + 1.0) * width / 2.0);
+          int y1 = (int) ((v1.getY() + 1.0) * height / 2.0);
+          try {
+            render.lineThird(x0, y0, x1, y1, image, Color.WHITE);
+          } catch (Exception e) {
+            System.out.println("START");
+            System.out.println(x0);
+            System.out.println(x1);
+            System.out.println(y0);
+            System.out.println(y1);
+            System.out.println("END");
+          }
+        }
+      }
+
       image = render.flipVertically(image);
       int [] pixels = image.getRGB(0, 0, width, height, null, 0, width);
 
@@ -30,9 +60,28 @@ public class JTinyRenderer {
       FileOutputStream fos = new FileOutputStream(path.replace(".bmp", ".tga"));
       fos.write(buffer);
       fos.close();
-    }
-    catch (IOException e) {
+
+    } catch (IOException e) {
       e.printStackTrace();
     }
+
+//    try {
+//      int width = 100;
+//      int height = 100;
+//      BufferedImage image = new BufferedImage(100, 100, TYPE_INT_RGB);
+//      render.lineThird(13, 20, 80, 40, image, Color.WHITE);
+//      render.lineThird(20, 13, 40, 80, image, Color.RED);
+//      render.lineThird(80, 40, 13, 20, image, Color.RED);
+//      image = render.flipVertically(image);
+//      int [] pixels = image.getRGB(0, 0, width, height, null, 0, width);
+//
+//      byte [] buffer = TGAWriter.write(pixels, width, height, TGAReader.ARGB);
+//      FileOutputStream fos = new FileOutputStream(path.replace(".bmp", ".tga"));
+//      fos.write(buffer);
+//      fos.close();
+//    }
+//    catch (IOException e) {
+//      e.printStackTrace();
+//    }
   }
 }
--- src/main/java/com/robotzero/Render.java
+++ src/main/java/com/robotzero/Render.java
@@ -22,6 +22,52 @@ public class Render {
     }
   }
 
+  void lineThird(int x0, int y0, int x1, int y1, BufferedImage image, Color color) {
+    boolean steep = false;
+    if (Math.abs(x0 - x1) < Math.abs(y0 - y1)) {
+      int tempx0 = x0;
+      int tempy0 = y0;
+      int tempx1 = x1;
+      int tempy1 = y1;
+      x0 = tempy0;
+      y0 = tempx0;
+      x1 = tempy1;
+      y1 = tempx1;
+      steep = true;
+    }
+
+    if (x0 > x1) {
+      int tempx0 = x0;
+      int tempy0 = y0;
+      int tempx1 = x1;
+      int tempy1 = y1;
+      x0 = tempx1;
+      x1 = tempx0;
+      y0 = tempy1;
+      y1 = tempy0;
+    }
+
+    int dx = x1 - x0;
+    int dy = y1 - y0;
+
+    int derror2 = Math.abs(dy) * 2;
+    float error2 = 0;
+    int y = y0;
+    for (int x = x0; x <= x1; x++) {
+      if (steep) {
+        image.setRGB(y, x, color.getRGB()); // if transposed, de−transpose
+      } else {
+        image.setRGB(x, y, color.getRGB());
+      }
+
+      error2 += derror2;
+      if (error2 > dx) {
+        y += (y1 > y0) ? 1 : -1;
+        error2 -= dx * 2;
+      }
+    }
+  }
+
   public BufferedImage flipVertically(BufferedImage image) {
     AffineTransform tx = AffineTransform.getScaleInstance(1, -1);
     tx.translate(0, -image.getHeight());
@@ -30,3 +76,23 @@ public class Render {
     return image;
   }
 }
+
+//  bool steep = false;
+//    if (std::abs(x0-x1)<std::abs(y0-y1)) { // if the line is steep, we transpose the image
+//    std::swap(x0, y0);
+//    std::swap(x1, y1);
+//    steep = true;
+//    }
+//    if (x0>x1) { // make it left−to−right
+//    std::swap(x0, x1);
+//    std::swap(y0, y1);
+//    }
+//    for (int x=x0; x<=x1; x++) {
+//    float t = (x-x0)/(float)(x1-x0);
+//    int y = y0*(1.-t) + y1*t;
+//    if (steep) {
+//    image.set(y, x, color); // if transposed, de−transpose
+//    } else {
+//    image.set(x, y, color);
+//    }
+//    }
